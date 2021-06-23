5bf99343e9d05c88250894c6036e49e969c9c209
Roman <designer_79@mail.ru>
2021-06-23 08:10:00 +0300

    изменения в приложении, убрал верхнюю строку главного экрана

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'internal/application.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark));
   runApp(Application());
 }
